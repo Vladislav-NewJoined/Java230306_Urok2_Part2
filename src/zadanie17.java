4eba956f005d5dda9cacf8ecd2449733f5758a11
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-03-10 12:14:53 +0300

    Initial commit

--- src/zadanie17.java
+++ src/zadanie17.java
@@ -1,5 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class zadanie17 {
+    //17. Напишите программу, сохраняющую в файл статью из википедии «Проблема 2000 года». Прочитайте её.
+
+    public static void main(String[] args) throws IOException {
+        String result = downloadWebPage("https://ru.wikipedia.org/wiki/%D0%9F%D1%80%D0%BE%D0%B1%D0%BB%D0%B5%D0%BC%D0%B0_2000_%D0%B3%D0%BE%D0%B4%D0%B0");
+        System.out.println(result);
+    }
+
+    private static String downloadWebPage(String url) throws IOException {
+        StringBuilder result = new StringBuilder();
+        String line;
+        URLConnection urlConnection = new URL(url).openConnection();
+        try (InputStream is = urlConnection.getInputStream();
+             BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+            while ((line = br.readLine()) != null) {
+                result.append(line);
+            }
+        }
+        return result.toString();
     }
 }
